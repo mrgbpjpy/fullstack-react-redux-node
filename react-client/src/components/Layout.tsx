9ec12d62d5fc83b674413f403cb67ce2fb97c6c5
mrgbpjpy <mrgbpjpy@gmail.com>
2026-01-13 20:42:59 -0500

    Added Theme Slice/Reducer for BackGround and NavBar

--- react-client/src/components/Layout.tsx
+++ react-client/src/components/Layout.tsx
@@ -2,25 +2,43 @@ import {Link} from "react-router-dom";
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks";
 import { loadMe } from "../features/auth/authSlice";
+import { setTheme, type ThemeMode } from "../features/theme/themeSlice";
 
 export default function Layout({children}: {children: React.ReactNode}) {
     const dispatch = useAppDispatch();
     const token = useAppSelector((state) => state.auth.token);
+    const theme = useAppSelector((state) => state.theme.mode);
 
     useEffect(() => {
         if (token) {
             dispatch(loadMe(token));
         }
     },[token, dispatch])
+
+    const themeStyles = {
+        identity: {
+            bg: "#f5f7fb",
+            nav: "#111827",
+        },
+        system: {
+            bg: "#0f172a",
+            nav: "#020617",
+        },
+        activity: {
+            bg: "#fff7ed",
+            nav: "#7c2d12",
+        }
+    }[theme]
+
     return(
-        <div style={{ minHeight: "100vh", minWidth: "100vw", background: "#f5f7fb"}}>
+        <div style={{ minHeight: "100vh", minWidth: "100vw", background: themeStyles.bg}}>
         <nav
             style={{
-                background: "#111827",
+                background: themeStyles.nav,
                 color: "white",
                 padding: "12px 20px",
                 display: "flex",
-                gap: 16,
+                gap: 25,
             }}
         >
             <Link style={{ color: "white", textDecoration: "none"}} to="/login">
@@ -32,7 +50,19 @@ export default function Layout({children}: {children: React.ReactNode}) {
             <Link style={{ color: "white", textDecoration: "none"}} to="/dashboard">
                Dashboard
             </Link>
+            <div style={{ borderStyle:"groove", paddingLeft: 35, paddingRight: 35, paddingTop: 2,paddingBottom: 2, display: "flex", gap: 20  }}>
+            <strong>Theme:</strong>
+            <select
+                value={theme}
+                onChange={(e) => dispatch(setTheme(e.target.value as ThemeMode))}
+            >
+               
+                <option value="identity">Identity</option>
+                <option value="system">System</option>
+                <option value="activity">Activity</option>
 
+            </select>
+            </div>
         </nav>
         <main
             style={{
@@ -41,6 +71,7 @@ export default function Layout({children}: {children: React.ReactNode}) {
                 padding: 20,
             }}
         >
+            
             {children}
         </main>
         </div>
